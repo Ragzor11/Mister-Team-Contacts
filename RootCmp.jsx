657edf286a3a9c31f890547c636c35fc5848f930
DenisBerezin <compA321@gmail.com>
2023-09-19 15:25:11 +0300

    initial commit

---
+++ RootCmp.jsx
@@ -0,0 +1,22 @@
+const { Route, Routes } = ReactRouterDOM
+const Router = ReactRouterDOM.HashRouter
+
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { About } from './views/About.jsx'
+import { Home } from './views/Home.jsx'
+import { AppIndex } from './views/AppIndex.jsx'
+
+export function App() {
+    return (
+        <Router>
+            <section className="app">
+                <AppHeader />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/app" element={<AppIndex />} />
+                </Routes>
+            </section>
+        </Router>
+    )
+}
